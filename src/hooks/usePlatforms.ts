84b9b195da9a3d7a471efc18c2f6f1e91d79c75f
Nisarg Patel <nisarg.phpdeveloper@gmail.com>
2024-08-08 14:50:20 +1200

    Refractor - Removing Duplicate Interface

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -2,7 +2,7 @@ import { FetchResponse } from "../services/api-client";
 import platforms from "../data/platforms";
 import { useQuery } from "@tanstack/react-query";
 import apiClient from "../services/api-client";
-interface Platform {
+export interface Platform {
   id: number;
   name: string;
   slug: string;
